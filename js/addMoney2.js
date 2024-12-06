16b1f207dd2bcd175beb42cdfb5abeac387c04f6
mddalimmiah <mddalimsaif@gmail.com>
2024-12-06 22:29:10 +0600

    added add money and cash out validation and transaction history

--- js/addMoney2.js
+++ js/addMoney2.js
@@ -4,6 +4,11 @@ document.getElementById('btn-add-money')
     event.preventDefault();
 
     const addMoney=getInputFieldValueById('input-add-money');
+
+    if(isNaN(addMoney)){
+        alert('failed to add money');
+        return; // return na korle habijabi dekhabe transaction hoye gece
+    }
     const pinNumber=getInputFieldValueById('input-pin-number');
     // console.log('input add money inside addmoney2',addMoney, pinNumber);
 
@@ -14,6 +19,15 @@ document.getElementById('btn-add-money')
         const newBalance=balance + addMoney;
         // console.log(newBalance, addMoney)
         document.getElementById('account-balance').innerText=newBalance;
+
+        // add to transaction history:
+
+        const p=document.createElement('p');
+        p.innerText=` Added: ${addMoney} Tk. New Balance: ${newBalance}`;
+        console.log(p)
+
+        // should be a common function:
+        document.getElementById('transaction-section').appendChild(p);
     }
     else{
         alert('failed to add money! please try again later!')
--- js/cashOut.js
+++ js/cashOut.js
@@ -8,11 +8,31 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
     const cashPinNumber=getInputFieldValueById('input-cash-out-pin');
 
     // console.log('cash out clicked', cashOut, cashPinNumber)
+
+    if(isNaN(cashOut)){
+        alert('failed to cash out');
+        return;
+    }
     if(cashPinNumber===1234){
         const balance=getTextFieldValueById('account-balance');
+        if(cashOut>balance){
+            alert('You do not have Enough Money to cash out!');
+            return;
+        }
         const newBalance=balance - cashOut;
         document.getElementById('account-balance').innerText=newBalance;
         console.log('balance here', balance, newBalance)
+        
+        // add to transaction history:
+
+        const div=document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML=`
+        <h2 class="text-2xl font-bold"> Cash Out </h2>
+        <p> ${cashOut} .withdraw. new Balance: ${newBalance} </p>
+        `
+        // appned child kore dibo tahole dekha jabe 
+        document.getElementById('transaction-section').appendChild(div);
     }
     else{
         alert('Failed to cash out!, please try again later!');
